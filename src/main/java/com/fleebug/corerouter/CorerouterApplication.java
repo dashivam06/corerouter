f6cdf9b9f8c616ae74b0857e6a211389e1860013
dashivam06 <thakurshivam202063@gmail.com>
2026-03-25 01:14:21 +0545

    Update Application Insights configuration and dependencies; refactor logging settings for improved clarity

--- src/main/java/com/fleebug/corerouter/CorerouterApplication.java
+++ src/main/java/com/fleebug/corerouter/CorerouterApplication.java
@@ -2,12 +2,13 @@ package com.fleebug.corerouter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 
 @SpringBootApplication
 @EnableScheduling
 public class CorerouterApplication {
-
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(CorerouterApplication.class, args);
 	}
 
